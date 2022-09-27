7f4e42686ccc8ae06af7bb8a208c8b8547c90c7a
HasrilMuhammadIbal <113493778+HasrilMuhammadIbal@users.noreply.github.com>
2022-09-27 12:31:04 +0700

    Add files via upload
    jdk-17.0.4.1
    Spring Boot 2.7.4
    Packaging jar
    Java 17

---
+++ user-service/src/main/java/com/tk2b/user/servive1/service/UserService.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tk2b.user.servive1.service;
+
+import com.tk2b.user.service1.dto.User;
+import java.util.List;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class UserService {
+
+    public User deleteUser(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public User updateUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public List<User> getAllUsers() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public User getUser(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void addUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
